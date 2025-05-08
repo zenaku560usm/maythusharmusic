50e88543d4b8c7a70892faf3121d9cf53d247a06
zenaku560usm <pyaesone45usm@gmail.com>
2025-05-08 23:04:55 +0630

    Update start.py

--- maythusharmusic/plugins/bot/start.py
+++ maythusharmusic/plugins/bot/start.py
@@ -47,7 +47,7 @@ async def start_pm(client, message: Message, _):
                 )
             return
         if name[0:3] == "inf":
-            m = await message.reply_text("🔎")
+            m = await message.reply_sticker("CAACAgUAAxkBAAIBkmgc1QyyhxYUA3FHeJXbAusu_LlKAAKZGQACDpXoVIEmW7x1SB0JNgQ")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
