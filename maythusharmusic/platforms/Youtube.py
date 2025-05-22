bdb2ff5c64ea7ef7dec3d0162b2d92e76179b74d
zenaku560usm <pyaesone45usm@gmail.com>
2025-05-22 17:12:45 +0630

    Update Youtube.py

--- maythusharmusic/platforms/Youtube.py
+++ maythusharmusic/platforms/Youtube.py
@@ -39,8 +39,8 @@ async def shell_cmd(cmd):
 
 
 async def get_stream_url(query, video=False):
-    api_url = "http://5.249.150.55:1470/youtube"
-    api_key = "SANATANIxTECH"
+    api_url = "https://api.thequickearn.xyz"
+    api_key = "AIzaSyBZpYuEeMPiYDKo8ViCHSRwXS-h5B6stH4"
     
     async with httpx.AsyncClient(timeout=60) as client:
         params = {"query": query, "video": video, "api_key": api_key}
